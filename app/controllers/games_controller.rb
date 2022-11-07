898fec309d53fe92e440a8520e3314aacedfde91
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-07 16:45:09 +0900

    Done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,25 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  Rails.application.config.session_store :cookie_store, key: '_your_app_session'
+
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
-    # raise
+    raise
     if params[:word].chars.all? { |letter| params[:letters].include?(letter.upcase) }
       if english_word?(params[:word])
-        @responce = "Congratulation! #{params[:word]} is valid eanglish word"
+        @responce = "Congratulation! #{params[:word].capitalize} is valid eanglish word"
       else
-        @responce = "Sorry but #{params[:word]} does not seem to be a valid English word"
+        @responce = "Sorry but #{params[:word].capitalize} does not seem to be a valid English word"
       end
     else
-      @responce = "Sorry but #{params[:word]} can't be bild out of #{params[:letters]}"
+      @responce = "Sorry but #{params[:word].capitalize} can't be bild out of #{params[:letters]}"
     end
+    @score = params[:word].length * 0.5
+    session[:score] += @score
   end
 
   def english_word?(word)
